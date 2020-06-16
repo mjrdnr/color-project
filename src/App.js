ead66069073b20ed4a1386e54d4534339ba961c3
mjrdnr <mordinario1002@yahoo.com>
2020-06-16 17:23:51 +0800

    Add ColorHelper and unstyled color slider

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './App.css';
 import Palette from './Palette';
 import seedColors from './seedColors';
+import { generatePalette } from './ColorHelpers';
 
 function App() {
   return (
     <div>
-      <Palette {...seedColors[4]} />
+      <Palette palette={generatePalette(seedColors[4])} />
     </div>
   );
 }
